6244d582980df34bf6b7afaca5a24eff7046f85c
Jackie <118042037+jackiezheng1998@users.noreply.github.com>
2023-02-17 21:48:08 -0500

    Created my react portfolio

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Healthy Gigs",
+    subtitle: "HTML, CSS, JS",
+    description:
+      "Healthy Gigs is a website dedicated to finding safe events during the COVID-19 pandemic. The website allows the user to search for events based on type, location or date. When the event results display, the user is also presented with information on COVID levels in the area where the events are.",
+    image: "./healthy-gigs.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Booty Camp",
+    subtitle: "Javascript, Node.js, Handlebars, MySQL, Sequelize",
+    description:
+      "Welcome to Booty Camp! With our fitness app, you are going to maximize your performace. With our app, you can freely choose between the work outs you want to do, and our app gives out demo videos to show you the correct way to perform workout routines.",
+    image: "./bootycamp.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "How to Burn Money",
+    subtitle: "Javascript, React, MongoDB, GraphQL, Node.js, Express",
+    description:
+      "Welcome to the ultimate expense tracker, over here you can maximize your saving and decrease the unnecessary expenses! With this app, you can easily calculate how much money you make, spend, and invest. With our visualization tool, you can easily see the portion of each spending.",
+    image: "./How-to-burn-money.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
